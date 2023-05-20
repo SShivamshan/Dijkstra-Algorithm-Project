32d41473871073252e08cc3d1b8c5673fe8fbafc
Shivamshan Sivanesan <116385693+SShivamshan@users.noreply.github.com>
2023-05-20 21:24:03 +0200

    Changes
    Changes to the compute_travel function and add a reverse path to retrieve the path from the end node to start node instead of doing it inside the compute travel function

---
+++ station.hpp
@@ -0,0 +1,29 @@
+#ifndef STATION_H
+#define STATION_H
+//#include "Generic_connection_parser.hpp"
+#include "Generic_mapper.hpp"
+
+// as the class Generic_connection_parser inherits from Generic_station_parser this can create ambiguity 
+// from the stations_hashmaps variable and thus inherits only from Generic_connection_parser
+// same way Station parser inherits directly from Generic mapper which inherits from Generic_connection_parser which inherits from Generic_station_parser
+class Station_parser : public travel::Generic_mapper{
+    protected:
+        void read_stations(const std::string& _filename) override; // implementation of the pure virtual function of the class 
+                                                                   // Generic_station_parser
+        void read_connections(const std::string& _filename) override;// implementation of the pure virtual function of the class 
+                                                                   // Generic_connection_parser
+    public:
+        // getters for read_connections, read_stations functions to be used outside the class
+        void Useread_stations(const std::string& _filename); // to access the read_stations function which is protected
+        void Useread_connections(const std::string& _filename);
+    
+        std::vector<std::pair<uint64_t,uint64_t> > compute_travel(uint64_t _start, uint64_t _end);
+        std::vector<std::pair<uint64_t,uint64_t> > compute_and_display_travel(uint64_t _start, uint64_t _end);
+        // Function over loading 
+        std::vector<std::pair<uint64_t,uint64_t> > compute_travel(const std::string& _start, const std::string& _end);
+        std::vector<std::pair<uint64_t,uint64_t> > compute_and_display_travel(const std::string& _start, const std::string& _end);
+    private:
+        std::vector<std::pair<uint64_t,uint64_t> > reverse_path(uint64_t start, uint64_t _end,std::unordered_map<uint64_t,uint64_t> &prev); 
+};
+
+#endif
